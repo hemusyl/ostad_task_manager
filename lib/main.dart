852845857e6aa5c26c0c38d478ec89ecb457ffff
hemusyl <humayun1278@gmail.com>
2025-08-07 16:42:41 -0400

    Main Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_task_manager/app.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(TaskManagerApp());
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        body:GoogleMap(initialCameraPosition: initialCameraPosition)
+      ),
+    );
+  }
+}
+
+
 
